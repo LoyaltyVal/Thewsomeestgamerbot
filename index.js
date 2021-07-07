eb104fcc7cd242e74bd20affb9280f6bc8d055e2
steamworksmc <58116838+steamworksmc@users.noreply.github.com>
2021-07-07 17:34:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
+require("dotenv").config()
 const Discord = require('discord.js')
 const client = new Discord.Client();
-
-// const token = require('./token.json')
 
 client.on('ready', () => {
     console.log('Thewsomeestgamerbot is now Online');
@@ -10,44 +9,25 @@ client.on('ready', () => {
 
 client.on('message', async (message) => {
     if(message.author.bot) return;
-
-    // let prefixes = ["!", ""];
+    
     let prefix = "!"
 
     if(message.content.indexOf(prefix) !== 0) return;
 
     const args = message.content.slice(prefix.length).split(' ');
-    // array = ["my", "dog", "is", "nice"]
     const command = args.shift().toLowerCase();
 
     if(command == 'hi') {
         return message.channel.send(':wave:');
-    }
-
-
-    if(command == 'partner') {
+    } else if(command == 'partner') {
         return message.channel.send("This server doesn't have any partners yet please wait and see what partners we have next!")
-    }
-
-    if(command == 'youtube') {
+    } else if(command == 'youtube') {
         return message.channel.send('https://www.youtube.com/channel/UCnWjs3WqMz2vRKSGIDpcMgQ')
     }
     
     if(command == 'commands') {
-        return message.channel.send('!commands !hello !partner !youtube')
-        if(command == 'commands') {
-            return message.channel.send('!commands !hello !partner !youtube')
-            {
-                
-            }
-            if(command =='ticket')
-            MessageEvent.channel.send('Work in progress please wait under worked on wore :D if you need anymore infoermation messege thewsomeestgamer!')
-            
-            
-        }
+        return message.channel.send('```\n' + ['hi', 'partner', 'youtube'].join('\n') + "```");
     }
-});[
-    
-]
+});
 
 client.login(process.env.TOKEN);
